bf2a56488b6c12974f90ce3aa07b43fcb38546b5
magenx <designelo@gmail.com>
2015-10-28 18:43:35 +0000

    Update chroot.sh

--- php-fpm.d/chroot.sh
+++ php-fpm.d/chroot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ## create files for chroot
 
-mkdir -p usr/share
-mkdir -p {tmp,dev,pki,etc,lib64}
+mkdir -p {etc/pki,usr/share,tmp,dev,lib64}
 cp /etc/nsswitch.conf  etc/
 cp /etc/resolv.conf  etc/
-cp -rf /etc/pki/{CA,ca-trust,tls,nssdb} pki/
+cp -rf /etc/pki/{CA,ca-trust,tls,nssdb} etc/pki/
 cp /lib64/{libsoftokn3.*,libnss*,libfreebl*} lib64/
 cp -rf /usr/share/zoneinfo  usr/share/
 mknod -m 644 dev/urandom c 1 9
