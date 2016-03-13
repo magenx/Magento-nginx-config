39bc9e859b04515a27a2bc7eadf245365fcaccc5
magenx <magenx@users.noreply.github.com>
2016-03-13 16:48:25 +0000

    Delete full_chroot_update.sh

--- php-fpm.d/full_chroot_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## CHROOT SIMPLE QUICK UPDATE
-## running from chroot folder
-
-\cp -Lrf {/usr/lib64/,/lib64/}{libnss*,libsoftokn*,libfreebl*} lib64/
-\cp -rf /etc/pki/{CA,ca-trust,tls,nssdb}  etc/pki/
