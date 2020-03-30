38188eee27dc50e79ac949c58cf262e5c3357c80
MagenX <magenx@users.noreply.github.com>
2020-03-30 11:34:11 +0100

    Update m2_config_install.sh

--- m2_config_install.sh
+++ m2_config_install.sh
@@ -2,35 +2,27 @@
 #### Install nginx configuration
 #### IT WILL REMOVE ALL CONFIGURATION FILES THAT HAVE BEEN PREVIOUSLY INSTALLED.
 
-NGINX_EXTRA_CONF="assets.conf error_page.conf extra_protect.conf maintenance.conf php_backend.conf maps.conf phpmyadmin.conf varnish_proxy.conf setup.conf pagespeed.conf status.conf"
-NGINX_EXTRA_CONF_URL="https://raw.githubusercontent.com/magenx/nginx-config/master/magento2/conf_m2/"
+GITHUB_REPO_API_URL="https://api.github.com/repos/magenx/Magento-nginx-config/contents/magento2"
 
 echo "---> CREATING NGINX CONFIGURATION FILES NOW"
 echo
-read -e -p "---> Enter your domain name (without www.): " -i "myshop.com" MY_DOMAIN
-read -e -p "---> Enter your web root path: " -i "/var/www/html/magento" MY_SHOP_PATH
-read -e -p "---> Enter your web user usually www-data (nginx for Centos): " -i "www-data" MY_WEB_USER
+read -e -p "---> Enter your domain name (without www.): " -i "myshop.com" MAGE_DOMAIN
+read -e -p "---> Enter your web user: " -i "www-data" MAGE_OWNER
+read -e -p "---> Enter your web root path: " -i "/home/${MAGE_OWNER}/public_html" MAGE_WEB_ROOT_PATH
 
-wget -qO /etc/nginx/fastcgi_params https://raw.githubusercontent.com/magenx/nginx-config/master/magento2/fastcgi_params
-wget -qO /etc/nginx/nginx.conf https://raw.githubusercontent.com/magenx/nginx-config/master/magento2/nginx.conf
-
-sed -i "s/www/sites-enabled/g" /etc/nginx/nginx.conf
+curl -o /etc/nginx/fastcgi_params https://raw.githubusercontent.com/magenx/Magento-nginx-config/master/magento2/fastcgi_params
+curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/magenx/Magento-nginx-config/master/magento2/nginx.conf
 
 mkdir -p /etc/nginx/sites-enabled
 mkdir -p /etc/nginx/sites-available && cd $_
-wget -q https://raw.githubusercontent.com/magenx/nginx-config/master/magento2/sites-available/default.conf
-wget -q https://raw.githubusercontent.com/magenx/nginx-config/master/magento2/sites-available/magento2.conf
+curl -s ${GITHUB_REPO_API_URL}/sites-available 2>&1 | awk -F'"' '/download_url/ {print $4 ; system("curl -sO "$4)}' >/dev/null
 
-sed -i "s/example.com/${MY_DOMAIN}/g" /etc/nginx/sites-available/magento2.conf
-sed -i "s/example.com/${MY_DOMAIN}/g" /etc/nginx/conf_m2/sslproxy.conf
-sed -i "s,/var/www/html,${MY_SHOP_PATH},g" /etc/nginx/sites-available/magento2.conf
-sed -i "s,user  nginx,user  ${MY_WEB_USER},g" /etc/nginx/nginx.conf
+sed -i "s/example.com/${MAGE_DOMAIN}/g" /etc/nginx/sites-available/magento2.conf
+sed -i "s,/var/www/html,${MAGE_WEB_ROOT_PATH},g" /etc/nginx/sites-available/magento2.conf
+sed -i "s,user  nginx,user  ${MAGE_OWNER},g" /etc/nginx/nginx.conf
 
 ln -s /etc/nginx/sites-available/magento2.conf /etc/nginx/sites-enabled/magento2.conf
 ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
 
 mkdir -p /etc/nginx/conf_m2 && cd /etc/nginx/conf_m2/
-for CONFIG in ${NGINX_EXTRA_CONF}
-do
-wget -q ${NGINX_EXTRA_CONF_URL}${CONFIG}
-done
+curl -s ${GITHUB_REPO_API_URL}/conf_m2 2>&1 | awk -F'"' '/download_url/ {print $4 ; system("curl -sO "$4)}' >/dev/null
